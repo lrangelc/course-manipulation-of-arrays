6663a5d7878e87ba384c497314b4b5e860f05459
Luis Rangel <luisrangelc@gmail.com>
2021-09-22 11:36:49 -0600

    fix function

--- server/14-flat.js
+++ server/14-flat.js
@@ -1,6 +1,21 @@
 console.clear();
 
-const matriz = [[1, 2, 3], [4, 5, 6, [4, 5, 6]], , [7, 8, 9, [1, 2, 3, [77, 88, 13]]]];
+const matriz = [[1, 2, 3], [4, 5, 6, [4, 5, 6]], , [7, 8, 9, [1, 2, 3, [77, 88, 13, [144, 666, 55]]]]];
+
+function flatArray2(array) {
+  const newMatriz = [];
+  array.forEach((element) => {
+    if (Array.isArray(element)) {
+      newMatriz.push(...flatArray2(element));
+    } else {
+      newMatriz.push(element);
+    }
+  });
+  return newMatriz;
+}
+
+const newMatriz1 = flatArray2(matriz);
+console.log('newMatriz1', newMatriz1);
 
 function getElements1(array, newArray) {
   array.forEach((element) => {
@@ -19,8 +34,8 @@ function flatArray(array) {
   return newMatriz;
 }
 
-const newMatriz = flatArray(matriz);
-console.log('newMatriz', newMatriz);
+const newMatriz0 = flatArray(matriz);
+console.log('newMatriz0', newMatriz0);
 
 // const newArray = [];
 // for (let i = 0; i < matriz.length; i++) {
@@ -36,3 +51,4 @@ console.log('newMatriz', newMatriz);
 
 console.log('flat', matriz.flat());
 console.log('flat level 3', matriz.flat(3));
+
